0ad6cddd5bff7a98e5765374db120baf49c53342
Yasin <yasinarabi1383@gmail.com>
2024-09-07 23:54:57 +0330

    feat:Created ViewSet for CarAds model

--- CarAds/views.py
+++ CarAds/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CarAds
+from .serializers import CarAdsSerializer
 
-# Create your views here.
+
+class CarAdsViewSet(viewsets.ModelViewSet):
+    queryset = CarAds.objects.all()
+    serializer_class = CarAdsSerializer
