fb5a24c9c6751604946c864c3250a247181dab88
Yasin <yasinarabi1383@gmail.com>
2024-09-07 20:19:34 +0330

    feat: creat CarAds Model

--- CarAds/models.py
+++ CarAds/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class CarAds(models.Model):
+    code = models.CharField(unique=True)
+    title = models.CharField(max_length=150)
+    year = models.IntegerField()
+    mileage = models.IntegerField()
+    location = models.CharField()
+    body_color = models.CharField()
+    body_type = models.CharField()
+    transmission = models.CharField()
+    price = models.BigIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'car_ads'
