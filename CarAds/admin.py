da9efc1d8ad7817f21e88a6719c5973c11286584
Yasin <yasinarabi1383@gmail.com>
2024-09-07 20:20:32 +0330

    feat: register CarAds model in django admin

--- CarAds/admin.py
+++ CarAds/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from CarAds.models import CarAds
+
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'location', 'price')
+
+    search_fields = ('title',)
+
+
+admin.site.register(CarAds, PostAdmin)
