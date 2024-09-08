b6734bc073f97c76c5ee67df3bf611b75cfe899f
Yasin <yasinarabi1383@gmail.com>
2024-09-09 00:57:39 +0330

    feat:Install SimpleJWT for JWT authentication

--- djangoWebAPI/settings.py
+++ djangoWebAPI/settings.py
@@ -106,3 +106,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'CarAds.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
