3cb0a2799410641a21ccf1dc8b0ee47622a95ea3
Yasin <yasinarabi1383@gmail.com>
2024-09-09 01:02:11 +0330

    feat:Register custom User model in admin panel

--- CarAds/admin.py
+++ CarAds/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from CarAds.models import CarAds
 
@@ -10,4 +12,25 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('title',)
 
 
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ('email',)
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+
 admin.site.register(CarAds, PostAdmin)
+admin.site.register(User, UserAdmin)
