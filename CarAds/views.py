18537c8600003ece42116d3ed9782d5ed5508454
Yasin <yasinarabi1383@gmail.com>
2024-09-09 01:03:13 +0330

    feat:Use CustomPagination for CarAdsViewSet

--- CarAds/views.py
+++ CarAds/views.py
@@ -1,14 +1,15 @@
-from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, generics
 from .serializers import CarAdsSerializer, UserSerializer
 
+from .pagination import CustomPagination
 from .models import CarAds
 
 
 class CarAdsViewSet(viewsets.ModelViewSet):
     queryset = CarAds.objects.all()
     serializer_class = CarAdsSerializer
+    pagination_class = CustomPagination
 
 
 User = get_user_model()
