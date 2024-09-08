01c2f2cee688e4e11771e1d093f1bde0e4f51f61
Yasin <yasinarabi1383@gmail.com>
2024-09-09 00:48:21 +0330

    feat:Add User serializer for authentication

--- CarAds/serializers.py
+++ CarAds/serializers.py
@@ -1,9 +1,35 @@
 from rest_framework import serializers
 
 from CarAds.models import CarAds
+from django.contrib.auth import get_user_model
 
 
 class CarAdsSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarAds
         fields = '__all__'
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password_confirm = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'password', 'password_confirm')
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')  # Remove the password_confirm field
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)  # Set the user's password
+        user.save()
+        return user
