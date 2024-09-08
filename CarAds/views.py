a43236099d141d476fedce936bf746f681083c07
Yasin <yasinarabi1383@gmail.com>
2024-09-09 00:52:13 +0330

    feat:Create authentication views for signup and login

--- CarAds/views.py
+++ CarAds/views.py
@@ -1,8 +1,19 @@
 from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, generics
+from .serializers import CarAdsSerializer, UserSerializer
+
 from .models import CarAds
-from .serializers import CarAdsSerializer
 
 
 class CarAdsViewSet(viewsets.ModelViewSet):
     queryset = CarAds.objects.all()
     serializer_class = CarAdsSerializer
+
+
+User = get_user_model()
+
+
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
