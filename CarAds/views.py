3cc5919d2179eea48591043d7628efa3798c7b14
Yasin <yasinarabi1383@gmail.com>
2024-09-09 03:15:51 +0330

    feat:Add IsAuthenticatedOrReadOnly permission to CarAdsViewSet

--- CarAds/views.py
+++ CarAds/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from .serializers import CarAdsSerializer, UserSerializer
 
 from .pagination import CustomPagination
@@ -10,6 +12,7 @@ class CarAdsViewSet(viewsets.ModelViewSet):
     queryset = CarAds.objects.all()
     serializer_class = CarAdsSerializer
     pagination_class = CustomPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 User = get_user_model()
