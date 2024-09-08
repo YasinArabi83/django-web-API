f23817345d0a98e0727fe64f7b2a6235065c22bf
Yasin <yasinarabi1383@gmail.com>
2024-09-09 00:59:01 +0330

    feat:Add authentication URLs

--- CarAds/urls.py
+++ CarAds/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CarAdsViewSet
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import SignupView
 router = DefaultRouter()
 router.register(r'car', CarAdsViewSet)
 urlpatterns = [
     path('', include(router.urls)),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
