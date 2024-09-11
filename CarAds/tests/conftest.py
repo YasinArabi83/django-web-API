121dea7bc47dbc469b03e9bd104c3f80f7162392
Yasin <yasinarabi1383@gmail.com>
2024-09-11 17:47:03 +0330

    feat:Add fixtures for user and car ads

---
+++ CarAds/tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+
+from CarAds.models import User, CarAds
+
+
+@pytest.fixture
+def car_ads_factory():
+    def make_car_ad(**kwargs):
+        defaults = {
+            'code': '1234',
+            'title': 'Test Car',
+            'year': 2023,
+            'mileage': 10000,
+            'location': 'Tehran',
+            'body_color': 'Red',
+            'body_type': 'Sedan',
+            'transmission': 'Automatic',
+            'price': 50000000,
+        }
+        defaults.update(kwargs)
+        return CarAds.objects.create(**defaults)
+
+    return make_car_ad
+
+
+@pytest.fixture
+def user_factory(db):
+    def create_user(**kwargs):
+        password = kwargs.pop('password', 'password123')
+        user = User.objects.create(**kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    return create_user
+
+
