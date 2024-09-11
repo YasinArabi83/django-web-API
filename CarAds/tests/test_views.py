edcbe39b7e6effd7bfd33603f3e36284266fd7a3
Yasin <yasinarabi1383@gmail.com>
2024-09-11 18:12:40 +0330

    feat:Add access control tests for CarAds views

---
+++ CarAds/tests/test_views.py
@@ -0,0 +1,34 @@
+from django.urls import reverse
+import pytest
+
+
+@pytest.mark.django_db
+def test_car_ads_list_view_unauthenticated(api_client):
+
+    response = api_client.get(reverse('carads-list'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_car_ads_create_view_unauthenticated(api_client, car_ads_data):
+
+    response = api_client.post(reverse('carads-list'), car_ads_data)
+    assert response.status_code == 401  # Unauthorized
+
+
+@pytest.mark.django_db
+def test_car_ads_create_view_authenticated(api_client, user_token, car_ads_data):
+
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + user_token['access_token'])
+    response = api_client.post(reverse('carads-list'), car_ads_data)
+    assert response.status_code == 201  # Created
+
+
+@pytest.mark.django_db
+def test_car_ads_list_authenticated(api_client, user_token, car_ads_data, car_ads_factory):
+
+    car_ads_factory(**car_ads_data)
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + user_token['access_token'])
+    response = api_client.get(reverse('carads-list'))
+    assert response.status_code == 200
+    assert len(response.data['results']) == 1
